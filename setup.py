166cc174c3c12ef9dfe09b20ea691881f9d185cb
Kamal Bin Mustafa <kamal@smach.net>
2011-10-22 10:57:32 +0800

    initial path and depends

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
-     )
+setup(
+    name='myshop',
+    version='1.0',
+    description='OpenShift App - LFC',
+    author='Kamal bin Mustafa',
+    author_email='example@example.com',
+    url='http://www.python.org/sigs/distutils-sig/',
+    install_requires=[
+        'Django==1.1.4',
+        'django-lfs',
+        'django-lfstheme',
+        'Pillow',
+    ],
+)
